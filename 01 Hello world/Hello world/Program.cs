ba524a70e74429e4a5084c93da8c0258a0e4e01d
Natalia Zorina <utoik@ya.ru>
2022-02-10 18:31:59 +0500

    hello world

---
+++ 01 Hello world/Hello world/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+
+namespace Hello_world
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            
+            WriteLine("Hello World!!");
+
+            Write("Hello");
+            Write(" ");
+            Write("World");
+            Write("!!!");
+
+            ReadKey();
+
+            
+
+        }
+    }
+}
