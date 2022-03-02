20b9d7087de53fee55eef8cda80d71f7be3ed593
Natalia Zorina <utoik@ya.ru>
2022-03-02 22:51:53 +0500

    Methods homework

---
+++ 05 Methods/01 Part/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Part
+{
+    internal class Program
+    {
+        static string[] StringToArray(string initialString)
+        {
+            string[] subs = initialString.Split(' ');
+            return subs;
+        }
+
+        static void PrintArray(string[] array) 
+        {
+            Console.WriteLine("\nСлова введенной строки: ");
+            foreach(var e in array) Console.WriteLine($"{e} ");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку, разделяя слова в ней с помощью пробела:");
+            string inputString = Console.ReadLine();
+
+            string[] array = StringToArray(inputString);
+
+            PrintArray(array);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 05 Methods/02 Part/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Part
+{
+    internal class Program
+    {
+        static string ReversWords(string inputPhrase)
+        {
+            string[] subs = inputPhrase.Split(' ');
+            string newString = ChangeWordsOrder(subs);
+            return newString;
+        }
+
+        static string ChangeWordsOrder(string[] initial)
+        {
+            string newstring = "";
+            for (int i = initial.Length; i > 0; i--) newstring += initial[i-1] + " ";
+
+            // Альтернативное решение:
+            //Array.Reverse(initial);
+            //foreach (var e in initial) newstring += e + " ";
+            return newstring;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите строку, разделяя слова в ней с помощью пробела:");
+            string inputString = Console.ReadLine();
+
+            string inputReverse = ReversWords(inputString);
+
+            Console.WriteLine(inputReverse);
+            Console.ReadKey();
+
+        }
+    }
+}
